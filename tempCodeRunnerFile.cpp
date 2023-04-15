791d56f064477a8e6325924ad5fad27c8fa05f45
Yashvin Goyal <100990409+YashvinGoyal@users.noreply.github.com>
2023-04-15 23:33:04 +0530

    Add files via upload

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,1 @@
-        vector<int>vec;
-        for (itr = mapp.begin(); itr != mapp.end(); itr++)
-        {
-            if (itr->second >= k)
-            {
-                vec.push_back(itr->first);
-                // cout << itr->second << " ";
-            }
-        }
-
-    }
-};
+2
