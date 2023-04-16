eee9a19c04de8a0cab35f1248e52a143cc738bae
Yashvin Goyal <100990409+YashvinGoyal@users.noreply.github.com>
2023-04-16 23:52:53 +0530

    Add files via upload

---
+++ revlinklis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+
+    }
+};
+int main()
+{
+
+    return 0;
+}
